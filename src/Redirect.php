f30e7b735041c5e06ad0ad87d2c98dfd583eb432
oscarotero <oom@oscarotero.com>
2018-01-24 21:21:19 +0100

    Code style fix

--- src/Redirect.php
+++ src/Redirect.php
@@ -4,12 +4,12 @@ declare(strict_types = 1);
 namespace Middlewares;
 
 use ArrayAccess;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use InvalidArgumentException;
 use Middlewares\Utils\Factory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 final class Redirect implements MiddlewareInterface
 {
